46f71b602ed3ee13b124d563d1f6c1a4858ee531
Samismail2010 <88996409+Samismail2010@users.noreply.github.com>
2022-12-07 00:04:43 -0600

    added download pdf feat

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,22 @@ import React, {useState} from 'react'
 import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa'
 import { HiOutlineMail } from "react-icons/hi";
 import {BsFillPersonLinesFill} from "react-icons/bs"
-import Logo from '../assets/si logo.png'
+import {Link} from 'react-scroll'
 
 const Navbar = () => {
     const [nav, setNav] = useState(false)
     const handleClick = () => setNav(!nav)
+    const onButtonClick = () => {
+        fetch('Resume2022.pdf').then(response => {
+            response.blob().then(blob => {
+                const fileURL = window.URL.createObjectURL(blob);
+                let alink = document.createElement('a');
+                alink.href = fileURL;
+                alink.download = 'Resume2022.pdf';
+                alink.click();
+            })
+        })
+    }
 
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#112858]
@@ -16,12 +27,29 @@ const Navbar = () => {
         </div>
         {/* Menu */}
             <ul className='hidden md:flex'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Work</li>
-                <li>Contact</li>
-                <li>Resume</li>
+            <li><Link to="home" smooth={true} duration={500}>
+                    Home
+                </Link>
+            </li>
+            <li><Link to="about" smooth={true} duration={500}>
+                    About
+                </Link>
+            </li>
+            <li><Link to="skills" smooth={true}  duration={500}>
+                    Skills
+                </Link>
+            </li>
+            <li><Link to="work" smooth={true} duration={500}>
+                    Work
+                </Link>
+            </li>
+            <li><Link to="contact" smooth={true} duration={500}>
+                    Contact
+                </Link>
+            </li>
+            <li>
+                <button onClick={onButtonClick} type="submit" className='bg-pink-600 hover:bg-pink-500 font-bold rounded px-4 mx-auto flex items-center'> Download Resume</button>
+            </li>
             </ul>
        
         {/* hamburger */}
@@ -31,12 +59,29 @@ const Navbar = () => {
 
         {/* Mobile menu */}
        <ul className={!nav ? 'hidden': 'absolute top-0 left-0 w-full h-screen bg-[#112858] flex flex-col justify-center items-center'}>
-        <li className='py-6 text-3xl'>Home</li>
-        <li className='py-6 text-3xl'>About</li>
-        <li className='py-6 text-3xl'>Skills</li>
-        <li className='py-6 text-3xl'>Work</li>
-        <li className='py-6 text-3xl'>Contact</li>
-        <li className='py-6 text-3xl'>Resume</li>
+        <li className='py-6 text-3xl'>
+            <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+                    Home
+            </Link></li>
+        <li className='py-6 text-3xl'>
+            <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+                    About
+            </Link></li>
+        <li className='py-6 text-3xl'>
+            <Link onClick={handleClick} to="skills" smooth={true}  duration={500}>
+                    Skills
+            </Link></li>
+        <li className='py-6 text-3xl'>
+            <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+                    Work
+            </Link></li>
+        <li className='py-6 text-3xl'>
+            <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+                    Contact
+            </Link></li>
+        <li className='py-6 text-3xl'>
+           <button onClick={onButtonClick} type="submit" className='bg-pink-600 hover:bg-pink-500 font-bold rounded px-4 mx-auto flex items-center'> Download Resume </button>
+        </li>
        </ul>
 
        {/* Social icon */}
